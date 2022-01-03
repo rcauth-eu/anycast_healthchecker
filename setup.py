9946de533b9281adfc5e5d0ac3c0fa3b0266e8de
Mischa Salle <msalle@nikhef.nl>
2022-01-03 15:38:47 +0100

    Add systemd unit and sysconfig files

--- setup.py
+++ setup.py
@@ -6,15 +6,28 @@ import setuptools
 import sys
 
 if sys.argv[1]=='build':
-    print("Creating html files from rst files")
+    from os import system
     from docutils.core import publish_file
+    print("Creating html files from rst files")
     for s in ['README', 'TODO']:
         publish_file(source_path=s+'.rst',
                      destination_path=s+'.html',
                      writer_name='html')
+    # Use bird for the healthchecker special account
+    print("Updating systemd service file")
+    system("sed 's/^\(User\|Group\)=healthchecker$/\\1=bird/;"+
+           "s/^\(After\|Requires\)=network.service/\\1=network.target/' "+
+           "contrib/systemd/anycast-healthchecker.service > "+
+           "anycast-healthchecker.service")
+    # Also create empty sysconfig file, since systemd unit otherwise fails
+    print("Create empty sysconfig file")
+    f = open("anycast-healthchecker", "w")
+    f.write('#RUNASUSER="healthchecker"\n#RUNASGROUP="healthchecker"\n')
+    f.close()
 elif sys.argv[1]=='clean':
     from os import unlink
-    for f in ['README.html', 'TODO.html']:
+    for f in ['README.html', 'TODO.html',
+              'anycast-healthchecker.service', 'anycast-healthchecker']:
         try:
             unlink(f)
         except:
@@ -22,5 +35,9 @@ elif sys.argv[1]=='clean':
 
 
 setuptools.setup(
+    data_files = [
+        ('/usr/lib/systemd/system', ['anycast-healthchecker.service']),
+        ('/etc/sysconfig', ['anycast-healthchecker'])
+    ],
     setup_requires=['pbr'],
     pbr=True)
